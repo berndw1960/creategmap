b4d81eeeb5ab45452641f19e661d0b773420ea49
berndw1960 <weigelt.bernd@web.de>
2010-09-05 19:19:32 +0200

    removed many lines with code and comments

--- creategmap3.py
+++ creategmap3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = 0.60
+__version__ = "0.6.1"
 __author__ = "Bernd Weigelt, Jonas Stein"
 __copyright__ = "Copyright 2010, The OSM-TroLUG-Project"
 __credits__ = "Dschuwa"
@@ -10,17 +10,9 @@ __maintainer__ = "Bernd Weigelt, Jonas Stein"
 __email__ = "weigelt.bernd@web.de"
 __status__ = "preAlpha"
 
-
-import sys
-import os
-import http.client
-import re
-import tarfile
-import bz2
-
 """ 
   ===========VORSICHT ALPHA-STADIUM=================
-  creategmap.py, das script um ein gmapsupp.img für GARMIN-Navigationsgeräte
+  creategmap3.py, das script um ein gmapsupp.img für GARMIN-Navigationsgeräte
   zu erzeugen, z.B. Garmin eTrex Vista Hcx
   Ein Gemeinschaftsprojekt von Bernd Weigelt und Jonas Stein
   und als QCO Dschuwa
@@ -51,6 +43,11 @@ import bz2
   http://tuxcode.org/john/osbsql2osm/osbsql2osm-latest.tar.gz
 """
 
+import sys
+import os
+import http.client
+import re
+import tarfile
 
 # DEFs =============================================================================
 
@@ -117,106 +114,23 @@ def checkdir(dirtofind, solutionhint):
 
     return ExitCode
 
-  
 
-#def getmkgmap():
-#    target = http.client.HTTPConnection("www.mkgmap.org.uk")
-#    target.request("GET", "/snapshots/index.html")
-#
-#    htmlcontent =  target.getresponse()
-#
-#    print(htmlcontent.status, htmlcontent.reason)
-#
-#    data = htmlcontent.read()
-
-#    target.close()
-
-#    data = data.decode('utf8')
-    
-#    pattern = re.compile('mkgmap-r\d{4}')
-
-#    mkgmap_rev = sorted(pattern.findall(data), reverse=True)[1]
-
-#    os.system(("wget -N http://www.mkgmap.org.uk/snapshots/") + (mkgmap_rev) + (".tar.gz"))  
-#    tar = tarfile.open((work_dir) + (mkgmap_rev) + (".tar.gz"))
-#    tar.extractall()
-#    tar.close()
-    
-#    mkgmap = (work_dir) + (mkgmap_rev) + "/mkgmap.jar"
-#    print(mkgmap)
-    
-
-
-
-
-#def getsplitter():
-#    target = http.client.HTTPConnection("www.mkgmap.org.uk")
-#    target.request("GET", "/splitter/index.html")
-#
-#    htmlcontent =  target.getresponse()
-
-#    print(htmlcontent.status, htmlcontent.reason)
-
-#    data = htmlcontent.read()
-
-#    target.close()
-
-#    data = data.decode('utf8')
-    
-#    pattern = re.compile('splitter-r\d{3}')
-
-#    splitter_rev = sorted(pattern.findall(data), reverse=True)[1]
-   
-#    os.system(("wget -N http://www.mkgmap.org.uk/splitter/") + (splitter_rev) + (".tar.gz"))
-#    tar = tarfile.open((work_dir) + (splitter_rev) + (".tar.gz"))
-#    tar.extractall()
-#    tar.close()    
-    
-#    splitter = ((work_dir) + (splitter_rev) + "/splitter.jar")
-#    print(splitter)
-    
     
 # VARs =============================================================================
 
-web_help = "http://wiki.openstreetmap.org/wiki/User:Berndw"
-
-
-"""
-  Brauchen wir den eigentlich wirklich? 
-  Bernd: Glaube ich nicht, höchstens für das Debuggen
-"""  
-FailCounter = 0
 
 """
   Diese Funktion sollte bestehen bleiben, um entweder bei Erstbenutzung
   ausführliche Infos zu geben, und eventuell die Möglichkeit des Rücksetzen
   auf die Default-Einstellungen bei Fehlern zu bieten
 """
-firstrun = 0 ## nur zum Testen, default = 1 an dieser Stelle
+verbose = 0 ## nur zum Testen, default = 1 an dieser Stelle
 
 """ Arbeitsverzeichnis """
 
 
-work_dir = (os.environ['HOME'] + "/share/osm/map_build_test/") # Der Slash muss sein!!!
+work_dir = (os.environ['HOME'] + "/share/osm/map_build_test/") # Der letzte Slash muss sein!!!
 
-"""
-  Logfunktion sollte eventuell erweitert werden zur besseren Fehlerbehebung
-  Log-, bzw. verbosity-Level sollte einstellbar sein, zumindest variabel
-"""  
-## Log
-log = 0
-enable_log = 0
-disable_log = 0
-
-"""
-  Splitter und Mkgmap werden automatisch geholt und installiert
-  Optionen können verbessert oder entfernt werden.
-"""
-## Wo ist mkgmap
-#mkgmap = ((work_dir) + "mkgmap/mkgmap.jar")
-
-## Splitter
-#splitter = ((work_dir) + "splitter/splitter.jar")
 
 """
   Folgende Optionen sollten bei 'firstrun = 1' und Resets der Einstellungen 
@@ -227,44 +141,13 @@ disable_log = 0
 RAMSIZE = "-Xmx4000M"
 MAXNODES = "1000000"
 
-"""
-  Idee similar zu 'firstrun = 1' könnte zusammen gefasst werden
-  Oder gibt es was besseres?
-"""  
-## Interaktiver Modus
-abfrage = 0
 
 """
   Diese Optionen sollte per Konfigurationsdatei variabel sein
 """
 ## Standardkarte
 build_map = "germany"
- 
-## Velomap erstellen
-basemap = 0
 
-"""
-  Eigentlich geht es nicht ohne die folgenden drei Optionen, aber 
-  wenn kein Download gewünscht, dünne Leitung oder wg. Tests, sollte 
-  es die Möglichkeit geben, Arbeitsschritte zu 
-  überspringen, falls alle Bedingungen erfüllt sind.
-"""
-## Download der aktuellen Kartendaten
-download = 1
- 
-## OpenStreetBugs holen
-bugsholen = 1
- 
-## Tilesverzeichnis löschen
-rm_tiles = 1
-
-"""
-  Diese Option ist eigentlich sinnfrei, denn wenn nicht alles Teile 
-  vorhanden sind, klappt das ganze nicht und es gibt andere Fehlermeldungen
-  Kann also entfallen
-"""  
-## Falls was fehlt
-merge_error = "Zusammenfügen der Karte klappt nicht, da nicht alle Teile vorhanden sind!"
 
 
 ## Progamme und Verzeichnisse suchen
@@ -290,32 +173,21 @@ os.chdir(work_dir)
 """ 
   Eigene Einstellungen werden aus cgmap_py.conf gelesen
 
-  Konfiguraionsdatei umbenannt nach cgmap_py.conf um Konflikte mit der eventuell
+  Konfiguraionsdatei cgmap_py.conf um Konflikte mit der eventuell
   vorhandenen creategmap.conf des Bashscriptes zu vermeiden.
 """
 
+#checkfile("cgmap_py.conf", os.system("touch cgmap_py.conf"))
 
-checkfile("cgmap_py.conf", os.system("touch cgmap_py.conf"))
-
-#source $work_dir/creategmap.conf
  
  
 """
-  Optionen beim Programmstart stechen die Vorgaben aus dem Script 
-  oder der Konfigurationsdatei, sollten aber in der Konfiguration
-  gespeichert werden.
-  Aufrufe des Scripts ohne Optionen sollten , wenn sinnvoll, die 
-  vorangegangenen übernehmen.
-  Default (Vorschlag):
-  velomap  mit Bugs und Fixmes, wie beim Bashscript
-"""  
  
-#while test $# -gt 0
-#do
-#	case $1 in
-#	-h)
-print("""
-		creategmap [-options]
+  Default (Vorschlag):
+  velomap  mit Bugs und Fixmes
+
+
+		creategmap3.py [-options]
 
 		-i		interaktiv mit der Möglichkeit, Optionen zu ändern
 		-base		Basemap erstellen
@@ -326,79 +198,8 @@ print("""
 
 
 		Standard ist der Bau einer Velomap für mehr Kontrast auf kleinen Displays."
-		
-""")
-
-
-#		exit
-#	;;
- 
-#	-i)
-#		firstrun=1
-#	;;
- 
-#	-base)
-		# AIO-Basiskarte erstellen
-print(" Basemap - on ")
-#		basemap=1
-#	;;
-# 
-#	-nm)
-		# do not download german.osm
-print(" Download - off ")
-#		download=0
-#	;;
-# 
-#	-nb)
-		#  using old openstreetmap bugs
-print(" Bugsholen - off ")
-#		bugsholen=0
-#	;;
-# 
-#	-L)
-print(" Log - on ")
-#		enable_log=1
-#	;;
-#
-#	-l)
-print(" Log - off ")
-#		disable_log=0
-#	;;
-# 
-#	*)
-print(" error: invalid argument $1 ")
-#		exit
-#	;;  
-#	esac
-#	shift
-#done
 
 """
-  Logfunktion sollte an die Möglichkeiten von python angepasst werden,
-  firstrun = 1 sollte auf jeden Fall log = 1 mit verbose = 3 (alles) haben.
-  andere nur noch auf Wunsch.
-"""
-
-#if [ $enable_log = 1 ] ; then
-#	log=1
-#elif [ $disable_log = 1 ] ; then
-#	mv creategmap.conf creategmap.conf.sec
-#	sed '/log/d' creategmap.conf.sec > creategmap.conf	
-#	log=0
-#fi
-#
-#if [ $log = 1 ] ; then
-#	if [ -f cgm.log  ] ; then :
-#	else touch cgm.log
-#	fi
-#
-#	if [ -f creategmap.conf ] ; then
-#		mv creategmap.conf creategmap.conf.sec
-#		sed '/log/d' creategmap.conf.sec > creategmap.conf
-#	fi
-#	echo log=1 >> creategmap.conf
-#fi 
-# 
 
 
 
@@ -408,7 +209,7 @@ print(" error: invalid argument $1 ")
   Erfahrungswerte sind vorhanden, weitere sollten ermittelt werden.
 """
 # 
-if  firstrun == 1:
+if  verbose == 1:
     print(""" 
 		
 		Abhängig vom vorhandenen RAM muß die Menge des Speichers 
@@ -419,23 +220,11 @@ if  firstrun == 1:
 		Standard bei 2 GiB RAM ist die Vorgabe von "-Xmx2000M"
 		
     """)
-    print("            Vorgabewert: ", (RAMSIZE)) 
-    RAMSIZE = input("      Wieviel Speicher soll verwendet werden? ")
-    print("            Wahl:        ", (RAMSIZE))
-
-
-
-#	  if  [ -z $RAMSIZE ] ; then 
-#		  RAMSIZE=$RAMSIZE_OLD
-#	  fi
-#	  if [ -f creategmap.conf ] ; then
-#	    mv creategmap.conf creategmap.conf.sec
-#	    sed '/RAMSIZE/d' creategmap.conf.sec > creategmap.conf
-#	  fi
-#	  echo RAMSIZE=$RAMSIZE >> creategmap.conf
-
+    print("                 Vorgabewert: ", (RAMSIZE)) 
+    RAMSIZE = input("                 Wieviel Speicher soll verwendet werden? ")
+    print("                 Wahl:        ", (RAMSIZE))
  
-if  firstrun == 1:
+
     print(""" 
 		
 		Bei kleineren Karten können die Werte für die MAXNODES bei Splitter eventuell 
@@ -446,34 +235,10 @@ if  firstrun == 1:
 		4+GiB (-Xmx3000M) -->	1000000
 		
     """)       
-    print("            Vorgabewert: ", (MAXNODES))
-    MAXNODES = input("      Bitte Anzahl der gewünschten Nodes eingeben. ")
-    print("            Wahl:        ", (MAXNODES))
+    print("                 Vorgabewert: ", (MAXNODES))
+    MAXNODES = input("                 Bitte Anzahl der gewünschten Nodes eingeben. ")
+    print("                 Wahl:        ", (MAXNODES))
 
-
-#	  if  [ -z $MAXNODES ] ; then 
-#		  MAXNODES=$MAXNODES_OLD
-#	  fi
-#	  if [ -f creategmap.conf ] ; then
-#	    mv creategmap.conf creategmap.conf.sec
-#	    sed '/MAXNODES/d' creategmap.conf.sec > creategmap.conf
-#	  fi
-#	  echo MAXNODES=$MAXNODES >> creategmap.conf
-
-#fi
-
-
-"""
-  Wenn möglich sollte auch der Bau der kleineren Karten möglich sein.
-  Wäre eine Suchfunktion sinnvoll?
-  Oder eine Auswahl anhand von directory-listings?
-  Oder wäre das etwas für eine spätere grafische Version?
-"""
-
-#if [ $firstrun -eq 1 ] ; then
-#    map= 
-#    while [ -z $map ] ; do
-if  firstrun == 1:
     print(""" 
 	  
 	   
@@ -492,32 +257,13 @@ if  firstrun == 1:
                 Und dieser Vorgang dauert sehr lang und gelingt nicht unbedingt immer.
        	  
     """)
-    print("          Vorgabewert: ", (build_map))
-    build_map = input("     Bitte die gewünschte Karte eingeben ")
-    print("          Wahl:        ", build_map)
-#	  echo -n "       Ländernamen (englisch) ohne Dateiendung eingeben " [$default_map]
-#	  read map
-#	  if  [ -z $map ] ; then 
-#		  map=$default_map
-#	  fi
-#	  if [ -f creategmap.conf ] ; then
-#	    mv creategmap.conf creategmap.conf.sec
-#	    sed '/map/d' creategmap.conf.sec > creategmap.conf
-#	  fi
-#	  echo map=$map >> creategmap.conf
-#     done
-#fi
-
-"""
-  Einstellungen, die geändert werden sollten auf jeden fall ins Log
-"""  
-#if [ $log = 1 ] ; then
-#	time=`date '+%Y.%m.%d_%H:%M'`
-#	echo $time" cgm-version: " $version "   " $mkr "   " $spr "   " $map "   " $RAMSIZE "   " $MAXNODES >> cgm.log
-#fi 
+    print("                 Vorgabewert: ", (build_map))
+    build_map = input("                 Bitte die gewünschte Karte eingeben ")
+    print("                 Wahl:        ", build_map)
 
 
-#  get mkgmap
+
+##  get mkgmap
   
 
 target = http.client.HTTPConnection("www.mkgmap.org.uk")
@@ -537,7 +283,7 @@ tar.close()
 mkgmap = (work_dir) + (mkgmap_rev) + "/mkgmap.jar"
 
 
-#  get splitter
+##  get splitter
 
 target = http.client.HTTPConnection("www.mkgmap.org.uk")
 target.request("GET", "/splitter/index.html")
@@ -557,23 +303,6 @@ splitter = ((work_dir) + (splitter_rev) + "/splitter.jar")
 
 
 
-
-"""
-  Diese Funktion ist für mich wichtig, könnte aber in einem separaten Modul versteckt werden
-  Oder vorhandene eigene Änderungen haben eine höhere Priorität, dann braucht man die MKGMAP-Optionen
-  nicht zu verändern. Voraussetzung wäre aber ein separates Verzeichnis mit dem eigenen Styles.
-  Sollte im Log berücksichtigt werden.
-"""  
- 
-## auf meinem Rechner benutze ich eine alternative Einstellung für die Darstellung von Bugs und Fixmes
-## Erläuterungen finden sich auf der AIO-Wiki-Seite
- 
-#if [ -d mystyles ] ; then
-#      mapstyles=mystyles
-#else
-#      mapstyles=aiostyles
-#fi
- 
 """ 
   Styles-Vorlagen werden von GIT-Server der AIO-Karte geholt
   Aktualisierungen erfolgen automatisch
@@ -593,26 +322,6 @@ else:
     os.chdir(work_dir)
 
 
-
- 
-""" 
-  Arbeitsverzeichnis für Splitter wird erstellt...
-"""
- 
- 
-ExitCode = os.system("test -d tiles")
-
-if ExitCode == 0:
-    os.chdir("tiles")
-    os.system("rm -Rf *")
-    os.chdir(work_dir)
-	  
-else: 
-    os.mkdir("tiles")
-            
-  
-
- 
  
 """ 
   Die Höhenlinien werden einmalig geholt, hier nur für Deutschland, andere z.Z. nur manuell, 
@@ -647,7 +356,6 @@ os.system("bzcat osbdump_latest.sql.bz2 | osbsql2osm > OpenStreetBugs.osm")
 
 
 #  Download der OSM-Kartendaten von der Geofabrik
-  
 
 os.system("wget -N http://download.geofabrik.de/osm/europe/" + (build_map) + ".osm.bz2")
 
@@ -657,10 +365,21 @@ os.system("wget -N http://download.geofabrik.de/osm/europe/" + (build_map) + ".o
 ## freier Platz auf der Festplatte sein. Deutschland hat rund 10 GiB
 
 os.system("bunzip2 -k " + (build_map) + ".osm.bz2")
+ 
 
+##  Arbeitsverzeichnis für Splitter wird erstellt...
  
+ExitCode = os.system("test -d tiles")
+
+if ExitCode == 0:
+    os.chdir("tiles")
+    os.system("rm -Rf *")
+    os.chdir(work_dir)
+	  
+else: 
+    os.mkdir("tiles")
+             
 ## Splitten der Kartendaten, damit mkgmap damit arbeiten kann
- 
 
 os.chdir("tiles")
 os.system("java -ea " + (RAMSIZE) + " -jar " + (splitter) + " --mapid=63240023 --max-nodes=" + (MAXNODES) + " --cache=cache " + (work_dir) + (build_map) + ".osm")
@@ -670,9 +389,9 @@ os.chdir(work_dir)
 """ 
   Die Optionen für MKGMAP sind in externe Dateien ausgelagert
 
-  GBASEMAPOPTIONS =  -c basemap.conf
-  NOBASEMAPOPTIONS = -c fixme_buglayer.conf
-  VELOMAPOPTIONS = -c velomap.conf
+  GBASEMAPOPTIONS  =  -c basemap.conf
+  NOBASEMAPOPTIONS =  -c fixme_buglayer.conf
+  VELOMAPOPTIONS   =  -c velomap.conf
 """
 if  firstrun == 1:
     os.mkdir("gfixme")
@@ -684,11 +403,9 @@ if  firstrun == 1:
     os.mkdir("gboundary")
 
 ## Erstellen der Bugs- und FIXME-Layer für beide Kartenvarianten, Velomap oder AIO
- 
+
 os.system("rm -Rf gfixme/* gosb/* ")
- 
-#	echo $mapstyles 
- 
+
 os.chdir("gfixme")
 
 print(os.getcwd())
@@ -702,6 +419,7 @@ os.chdir(work_dir)
  
  
 ## Erstellen des Velomap-Layers
+
 os.system("rm -Rf gvelomap/* ") 
 
 os.chdir("gvelomap")
@@ -712,107 +430,33 @@ os.system("java -ea " + (RAMSIZE) + " -jar " + (mkgmap) + " -c " + (work_dir) + 
 os.chdir(work_dir)
  
  
-## oder der AIO-Karte, diese enthält zusätzlich einen Adress-, einen Grenz- und einen Maxspeed-Layer, jeweils abschaltbar im Kartenmenü des Navis.	  
- 
-#elif [ $basemap -eq 1 ] ; then
-#	dirs_basemap="gbasemap gaddr gboundary gmaxspeed"
-#	  for i in $work_dirs_basemap; do
-#	    if [ -d $i ] ; then
-#	      cd $i  
-#	      rm -Rf *
-#	      cd ..
-#	    else mkdir $i
-#	    fi
-#	  done
-#	  cd gbasemap
-#		  echo $PWD
+## Optionen für die Basemap, bitte stehenlassen, werden och gebraucht
+
 #		  java -ea $RAMSIZE -jar $mkgmap -c ../basemap.conf --style-file=../aiostyles/basemap_style --description='Openstreetmap' --family-id=4 --product-id=45 --series-name='OSMDEbasemap' --family-name=OSMBasemap --mapname=63240023 --draw-priority=10 $work_dir/tiles/*.osm.gz $work_dir/aiostyles/basemap.TYP
-#	  cd ../gaddr
-#		  echo $PWD
+
 #		  java -ea $RAMSIZE -jar $mkgmap -c ../fixme_buglayer.conf --style-file=../aiostyles/addr_style --description='Adressen' --family-id=5 --product-id=40 --series-name='OSMDEAddr' --family-name=OSMAdressen --mapname=63244023 --draw-priority=18  $work_dir/tiles/*.osm.gz $work_dir/aiostyles/addr.TYP
-#	  cd ../gboundary
-#		  echo $PWD
+
 #		  java -ea $RAMSIZE -jar $mkgmap -c ../fixme_buglayer.conf --style-file=../aiostyles/boundary_style --description='Grenzen' --family-id=6 --product-id=30 --series-name='OSMDEboundary' --family-name=OSMGrenzen  --mapname=63245023 --draw-priority=20 $work_dir/tiles/*.osm.gz $work_dir/aiostyles/boundary.TYP
-#	  cd ../gmaxspeed
-#		  echo $PWD
+
 #		  java -ea $RAMSIZE -jar $mkgmap -c ../fixme_buglayer.conf --style-file=../aiostyles/maxspeed_style--family-name=maxspeed --series-name="maxspeed" --family-id=84 --product-id=15 --series-name=OSMmaxspeed --family-name=OSMmaxspeed --mapname=63246023 --draw-priority=21 $work_dir/tiles/*.osm.gz $work_dir/aiostyles/maxspeed.TYP
-#	  cd ../  
-#fi
- 
+
  
 ## Zusammenfügen der Kartenteile
- 
-#if [ -f gvelomap/gmapsupp.img -a -f gosb/gmapsupp.img -a -f gfixme/gmapsupp.img -a $basemap -eq 0 ] ; then
 os.system("wine ~/bin/gmt.exe -jo gmapsupp.img gvelomap/gmapsupp.img gosb/gmapsupp.img gfixme/gmapsupp.img gcontourlines/gmapsupp.img")
-#elif [ -f gbasemap/gmapsupp.img -a -f gosb/gmapsupp.img -a -f gaddr/gmapsupp.img -a -f gfixme/gmapsupp.img -a -f gboundary/gmapsupp.img -a -f gmaxspeed/gmapsupp.img -a $basemap -eq 1 ] ; then
-#	$wine $work_dir/gmt/gmt.exe -jo gmapsupp.img gbasemap/gmapsupp.img gosb/gmapsupp.img gaddr/gmapsupp.img gfixme/gmapsupp.img gboundary/gmapsupp.img gmaxspeed/gmapsupp.img
-#else echo $merge_error ; exit
-#fi
- 
- 
-## Für einige Länder gibt es Höhenlinien
- 
-#if [ $map = germany ] ; then 
-#	mv gmapsupp.img flat_gmapsupp.img
-#	$wine $work_dir/gmt/gmt.exe -jo gmapsupp.img gcontourlines/gmapsupp.img flat_gmapsupp.img
-#elif [ -d hoehenlinien/$map ] ; then
-#	mv gmapsupp.img flat_gmapsupp.img
-#	$wine $work_dir/gmt/gmt.exe -jo gmapsupp.img hoehenlinien/$map/gmapsupp.img flat_gmapsupp.img
-#fi
- 
-#if [ -f flat_gmapsupp.img ] ; then
-#	rm flat_gmapsupp.img
-#fi
- 
-"""
-  Nach Erstellen einer(mehrere?) Sicherungen
-  kopieren der fertigen Karte ins Oberhaus mit aussagekräftigen Namen.
-""" 
-
-#cp gmapsupp.img ../$map.gmapsupp.img
- 
- 
-## Aufräumen
- 
-#rm -f $map.osm
- 
- 
-## Wenn wir bis hier gekommen sind, firstrun=0 und Leerzeilen entfernen
- 
-#mv creategmap.conf creategmap.conf.sec
-#sed '/firstrun/d;/^ *$/d' creategmap.conf.sec > creategmap.conf
-#echo firstrun=0 >> creategmap.conf
-#rm creategmap.conf.sec
 
 
 printinfo("Habe fertig!")
 
- 
+""" 
  
 ## Änderungen:
-# v0.6.0a1 - Beginn der Umstellung auf python, aktuell noch nicht benutzbar
 
-# v0.53- Erste Änderungen während der Betaphase, Texte und Grundeinstellungen
-#	 Logfunktion implentiert
+v0.6.1- first working version with python3, but there are a lot of things to do,
+       next is make it use startoptions and the cgmap_py.conf to remember
+       these options
+       there are many systemcalls, which only work on Linux, they must be changed
+       remove many comments and code from the bash, because they make it unreadable
 
-# v0.52- Anpassung an die Dokumentation von Dschuwa
- 
-# v0.51- CleanUps
- 
-# v0.50- Höhenlinien für einige Länder, die Daten gibt es bei http://openmtbmap.org/de/download/#hoehendaten
-#        Die Rohdaten müssen mit gmt.exe zu gmapsupp.img zusammengefügt, Anleitung liegt jeweils bei.
-#        Fertige gmapsupp.img sollten unter $work_dir/hoehenlinien/$map gespeichert werden, damit
-#        das Script sie findet.
-#
-#        Mit wine kann man das mit folgender Zeile erledigen, gmt.exe liegt dabei im Verzeichnis darüber:
-#       'wine ../gmt.exe  -j -f 5,25 -m HOEHE -o gmapsupp.img  *.img' 
-#       'wine gmt.exe -jo gmapsupp.img */gmapsupp.img' erstellt eine große Datei (660 MiB) für alle Länder
-#        kann man für die Europakarte verwenden, die ganze Karte wird etwa 4,8 GiB groß ;-)
-#
- 
- 
-# v0.49- Fehlerkorrekturen
- 
-# v0.47- firstrun = interaktiv
- 
-# v0.46- firstrun-Funktion für Grundeinstellungen
+v0.6.0 - Beginn der Umstellung auf python, aktuell noch nicht benutzbar
+
+"""
