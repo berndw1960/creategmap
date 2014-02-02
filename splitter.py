43cd9b5273a0bf3eea7885ff220dab34c993d7f1
Bernd Weigelt <weigelt.bernd@web.de>
2014-02-02 14:28:45 +0100

    after splitter error remove (buildmap)_areas.list if exist.

--- splitter.py
+++ splitter.py
@@ -67,16 +67,16 @@ def split():
   splitter_opts = (" --geonames-file=" + (WORK_DIR) + "cities15000.zip " +
                    " --mapid=" + config.get('mapid', (buildmap)) + "0001 " +
                    " --output=o5m " +
-                   " --keep-complete " +
                    " --precomp-sea=" + (WORK_DIR) + config.get('navmap', 'sea_rev') + ".zip "
                    " --write-kml=" + (buildmap) + ".kml "
                    " --max-nodes=" + config.get('splitter', 'maxnodes') +
+                   " --keep-complete " +
                    " --overlap=0 ")
 
   areas_list = (" --split-file=" + (WORK_DIR) + "areas/" + (buildmap) + "_areas.list ")
 
   BUILD_O5M = ((WORK_DIR) + "o5m/" + (buildmap) + ".o5m")
-  
+
   use_areas = config.get('splitter', 'use_areas')
   if use_areas == "yes":
     ExitCode = os.path.exists("areas/" + (buildmap) + "_areas.list")
@@ -93,7 +93,7 @@ def split():
     os.chdir("tiles")
     printwarning("no areas.list enabled, splitting the mapdata without it...")
     os.system((java_opts) + (log_opts) + (splitter_opts) + (BUILD_O5M))
-    
+
   os.chdir(WORK_DIR)
 
   ExitCode = os.path.exists("tiles/template.args")
@@ -101,8 +101,14 @@ def split():
     datei = open("tiles/" + (buildmap) + "_split.ready", "w")
     datei.close()
   else:
+    ExitCode = os.path.exists("areas/" + (buildmap) + "_areas.list")
+    if ExitCode == True:
+      os.remove("areas/" + (buildmap) + "_areas.list")
+      printwarning((buildmap) + "_areas.list removed, next build creates a new one")
     printerror("Splitter-Error!")
+    printerror("Please restart the buildprocess for " + (buildmap))
     quit()
+
   os.remove("tiles/" + (buildmap) + "_split.lck")
 
 
