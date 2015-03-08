61cc0c139b6b2c609c12fe7a11864f14182cd086
berndw1960 <weigelt.bernd@web.de>
2015-03-08 15:21:32 +0100

    add a fast build option to mapset

--- mapset.py
+++ mapset.py
@@ -8,6 +8,18 @@ import argparse
 
 WORK_DIR = os.environ['HOME'] + "/map_build/"
 
+"""
+test if a file or dir can be found at a predefined place
+raise message if fails and returns 1
+on success return 0
+"""
+
+if os.path.exists(WORK_DIR) == False:
+  printerror("Please create" + WORK_DIR)
+  quit()
+
+os.chdir(WORK_DIR)
+
 
 """
 set prefix for messages
@@ -22,6 +34,23 @@ def printwarning(msg):
 
 def printerror(msg):
   print("EE: " + msg)
+
+"""
+configparser
+
+"""
+
+def write_config():
+  with open('pygmap3.cfg', 'w') as configfile:
+    config.write(configfile)
+
+config = configparser.ConfigParser()
+import build_config
+
+if os.path.exists("pygmap3.cfg") == False:
+  build_config.create()
+
+config.read('pygmap3.cfg')
 
 
 """
@@ -39,53 +68,25 @@ parser = argparse.ArgumentParser(
           to create mapsets from a list
         '''))
 
-parser.add_argument('-ob', '--old_bounds', action="store_true", help="use the previous used bounds")
+## mapsets
 parser.add_argument('-a', '--add_mapset', dest='add_mapset', default='no')
-parser.add_argument('-r', '--rm_mapset', dest='rm_mapset', default='no')
 parser.add_argument('-lm', '--list_mapset', action="store_true", help="print out the mapset list")
-parser.add_argument('-nz', '--no_zip', action="store_true", help="don't zip the images after build")
+parser.add_argument('-r', '--rm_mapset', dest='rm_mapset', default='no')
 parser.add_argument('-d', '--del_mapset', action="store_true", help="deletes the whole list")
+parser.add_argument('-f', '--fastbuild', action="store_true", help="build a mapset for " + config['runtime']['default'])
+parser.add_argument('-s', '--set_default', dest='set_default', default='no', help="set region to fast build a mapset as new default")
+
+## pygmap3
+parser.add_argument('-ob', '--old_bounds', action="store_true", help="use the previous used bounds")
+parser.add_argument('-nz', '--no_zip', action="store_true", help="don't zip the images after build")
 parser.add_argument('-c', '--contourlines', action="store_true", help="enable countourlines layer creation")
 parser.add_argument('-st', '--stop_after', dest='stop_after', default='no', help='buildprocess stop after [tests|create|splitter|mkgmap]')
 parser.add_argument('-l', '--log', action="store_true", help="enable splitter and mkgmap logging")
-parser.add_argument('--svn', action="store_true", help="use svn versions of splitter and mkgmap")
-parser.add_argument('-mt', '--mkgmap_test', action="store_true", help="use a svn version of mkgmap like housenumbers2")
 parser.add_argument('-v', '--verbose', action="store_true", help="increase verbosity")
+parser.add_argument('-mt', '--mkgmap_test', action="store_true", help="use a svn version of mkgmap like housenumbers2")
+parser.add_argument('--svn', action="store_true", help="use svn versions of splitter and mkgmap")
 
 args = parser.parse_args()
-
-
-"""
-test if a file or dir can be found at a predefined place
-raise message if fails and returns 1
-on success return 0
-"""
-
-if os.path.exists(WORK_DIR) == False:
-  printerror("Please create" + WORK_DIR)
-  quit()
-
-os.chdir(WORK_DIR)
-
-
-"""
-configparser
-
-"""
-
-def write_config():
-  with open('pygmap3.cfg', 'w') as configfile:
-    config.write(configfile)
-
-config = configparser.ConfigParser()
-
-import build_config
-
-if os.path.exists("pygmap3.cfg") == False:
-  build_config.create()
-
-config.read('pygmap3.cfg')
-
 
 """
 set, edit or delete mapset list
@@ -135,64 +136,90 @@ if args.del_mapset:
   printwarning("mapset list deleted")
   quit()
 
+
+if args.set_default != "no":
+  if os.path.exists("poly/" + args.set_default + ".poly") == False:
+    print()
+    printerror((WORK_DIR) + "poly/" + args.set_default + ".poly not found... ")
+    printerror("please create or download "+ args.set_default + ".poly")
+    print()
+    quit()
+
+  config.set('runtime', 'default', args.set_default)
+  print(args.set_default + " set as new default region")
+
+  write_config()
+  quit()
+
+
+## build or additional option for pygmap3
+
+
 if args.stop_after:
-  stop = " --stop_after " + args.stop_after
+  stop = "-st " + args.stop_after + " "
 else:
-  stop = " "
+  stop = ""
 
 if args.contourlines:
-  cl = " -c "
+  cl = "-c "
 else:
-  cl = " "
+  cl = ""
 
 if args.mkgmap_test and config.has_option('runtime', 'mkgmap_test'):
-  mkgmap_test = " -mt "
+  mkgmap_test = "-mt "
 else:
-  mkgmap_test = " "
+  mkgmap_test = ""
 
 if args.svn:
-  svn = " --svn "
+  svn = "--svn "
 else:
-  svn = " "
+  svn = ""
 
 if args.no_zip:
-  zip = " "
+  zip = ""
 else:
-  zip = "  -z "
+  zip = "-z "
 
 if args.verbose:
-  verbose = " -v "
+  verbose = "-v "
 else:
-  verbose = " "
+  verbose = ""
 
 if args.log:
-  log = " --log "
+  log = "--log "
 else:
-  log = " "
+  log = ""
 
 if args.old_bounds:
-  ob = " -ob "
+  ob = "-ob "
 else:
-  ob = " "
+  ob = ""
 
-for buildmap in config['mapset']:
-  if config['mapset'][buildmap] == "yes":
-    if os.path.exists("stop") == True:
-      os.remove ("stop")
-      print()
-      printwarning("stopping build_process")
-      print()
-      quit()
-    command_line =  "pygmap3.py " + verbose + stop + cl + svn + mkgmap_test + log + zip + ob + " -b " + buildmap
+command_line =  "pygmap3.py " + verbose + stop + cl + svn + mkgmap_test + log + zip + ob
 
-    if args.verbose:
-      print(command_line)
+if args.fastbuild:
+  buildmap = config['runtime']['default']
+  if args.verbose:
+    print()
+    print(command_line + "-b " + buildmap)
 
-    os.system(command_line)
+  os.system(command_line + "-b " + buildmap)
 
-print()
-print("###### all mapsets successfully build! #######")
-print()
+else:
+  for buildmap in config['mapset']:
+    if config['mapset'][buildmap] == "yes":
+      if os.path.exists("stop") == True:
+        os.remove ("stop")
+        print()
+        printwarning("stopped build_process")
+        print()
+        quit()
+
+      os.system(command_line + "-b " + buildmap)
+
+  print()
+  print("###### all mapsets successfully build! #######")
+  print()
 
 
 quit()
