cd6b09b8cf93f86f37c4bd2d1faf6d80adaa159b
berndw1960 <weigelt.bernd@web.de>
2020-12-19 19:58:47 +0100

    remove uneeded code

--- planet_up.py
+++ planet_up.py
@@ -24,8 +24,6 @@ __status__ = "released"
 import os
 import argparse
 import configparser
-import datetime
-import shutil
 import build_config
 
 
@@ -94,8 +92,6 @@ parser = argparse.ArgumentParser(
          prog='PROG',
          formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('-cb', '--create_bounds', action="store_true",
-                    help="create new boundaries")
 parser.add_argument('-v', '--verbose', action="store_true",
                     help=False)
 
@@ -116,9 +112,6 @@ if not os.path.exists("pygmap3.cfg"):
 
 
 config.read('pygmap3.cfg')
-
-
-
 
 
 if not os.path.exists("planet/planet.osm.pbf"):
@@ -154,61 +147,6 @@ if os.path.exists("planet_new.osm.pbf"):
     if os.path.exists("planet.osm.pbf"):
         os.remove("planet_temp.osm.pbf")
 
-
-# create the bounds from planet
-
-
-if args.create_bounds:
-
-    command_line = ("osmfilter -v planet.osm.pbf  --keep-nodes= "
-                    + "--keep-ways-relations='boundary=administrative "
-                    + "=postal_code postal_code=' "
-                    + "--drop-tags='created_by= source= building= "
-                    + "=highway maxspeed= surface= oneway= note= natural="
-                    + " lit=' -o=bounds_data.o5m")
-
-    if args.verbose:
-        print()
-        print(command_line)
-        print()
-
-    os.system(command_line)
-
-    print()
-
-    mkgmap = WORK_DIR + config['mkgmap']['rev'] + "/mkgmap.jar "
-
-    if config['java']['agh'] == "1":
-        heap = " -XX:+AggressiveHeap "
-    else:
-        heap = config['java']['xmx'] + ' ' + config['java']['xms']
-
-    command_line = ("java -ea " +
-                    heap +
-                    " -cp " +
-                    mkgmap +
-                    " uk.me.parabola.mkgmap.reader.osm."
-                    + "boundary.BoundaryPreprocessor "
-                    + "bounds_data.o5m bounds ")
-    if args.verbose:
-        print()
-        print(command_line)
-        print()
-
-    os.system(command_line)
-
-    print()
-
-    # set date for info
-    today = datetime.datetime.now()
-    DATE = today.strftime('%Y%m%d')
-    zipf = "bounds-" + DATE
-    shutil.make_archive(zipf, 'zip', "bounds")
-    shutil.move(zipf + ".zip", WORK_DIR + "precomp")
-    shutil.rmtree("bounds")
-    os.remove("bounds_data.o5m")
-
-    os.chdir(WORK_DIR)
 
 info("Habe fertig!")
 quit()
